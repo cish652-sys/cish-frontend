14f6a1db22ab6b036e5df133b1b42acf129630b1
Shubham Singh <shubham.shubham23@gmail.com>
2025-09-23 00:28:30 +0530

    URL issue fixed

--- next.config.ts
+++ next.config.ts
@@ -6,3 +6,22 @@
 //   },};
 
 // export default nextConfig;
+
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'cish-web-steel.vercel.app',
+          },
+        ],
+        destination: 'https://cish.org.in/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
