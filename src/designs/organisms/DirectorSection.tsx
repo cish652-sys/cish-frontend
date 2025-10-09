1f8e50f0ff60343ca5b75dd64c1dbe079861e94b
Shubham Singh <shubham.shubham23@gmail.com>
2025-10-09 21:15:20 +0530

    test commit

--- src/designs/organisms/DirectorSection.tsx
+++ src/designs/organisms/DirectorSection.tsx
@@ -49,9 +49,7 @@ const DirectorSection: React.FC<DirectorSectionProps> = ({
 
   const content =
     directorInfo?.description ??
-    `Central Institute for Subtropical Horticulture Institute with a unique distinction of
-    working on the mango the king of fruit on one hand and guava the poor man’s apple on the
-    other...`;
+    `Central Institute for Subtropical Horticulture Institute with a unique distinction of working on the mango the king of fruit on one hand and guava the poor man’s apple on the other is placed on envious pedestal of having the world largest germplasm (755) in its field genebank. Being ever committed to challenging and tempting task of conserving our invaluable fast eroding genetic wealth of mango, during the period we through our dedicated team at the Institute could virtually ignite the passion of farmers and set the example to be emulated, for conserving the unique heirloom mango varieties by empowering the farmer through Society for Conservation of Biodiversity. Consequently we were able to submit 34 farmers and community varieties to PPV&fRA for registration, established community nurseries and outside community nursery produced more than 10000 grafts of non-commercial varieties. `;
 
   const imageSrc = directorInfo?.fileUrl ?? fallbackImageSrc;
 
