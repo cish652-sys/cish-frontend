8af6bca89917e99d388b387ecd9ded2ce15583d7
Shubham Singh <shubham.shubham23@gmail.com>
2025-10-26 16:44:27 +0530

    test

--- src/app/Commercialization/page.tsx
+++ src/app/Commercialization/page.tsx
@@ -55,7 +55,6 @@ export default function Commercialization() {
                 className="text-[#31572C] hover:text-[#599A5E]"
               >
                 APPLICATION FORM
-                
               </a>
             </li>
           </ul>
