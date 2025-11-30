0929ec697f2a2c55335d8bcd90aef59c96c012ea
Shubham Singh <shubham.shubham23@gmail.com>
2025-11-30 14:30:19 +0530

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-// turbopack: {
-//     root: "/home/basudev/Documents/cish-website",
-//   },};
-
-// export default nextConfig;
-
-
-// module.exports = {
-//   async redirects() {
-//     return [
-//       {
-//         source: '/:path*',
-//         has: [
-//           {
-//             type: 'host',
-//             value: 'cish-web-steel.vercel.app',
-//           },
-//         ],
-//         destination: 'https://cish.org.in/:path*',
-//         permanent: true,
-//       },
-//     ]
-//   },
-// }
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -33,40 +5,45 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'api.nationalfarmerportal.org',
-        port: '',
         pathname: '/uploads/**',
       },
       {
         protocol: 'http',
         hostname: '13.234.154.152',
-        port: '9000', 
+        port: '9000',
         pathname: '/nfp-bucket/**',
       },
       {
         protocol: 'https',
         hostname: 'example.com',
-        port: '',
         pathname: '/images/**',
       },
       {
         protocol: 'http',
         hostname: '13.234.154.152',
         port: '9000',
-        pathname: '/cish-bucket/**', // ✅ Add this line
+        pathname: '/cish-bucket/**',
       },
       {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '9000',
         pathname: '/nfp-bucket/**',
-      },
-     {
-        protocol: "http",
-        hostname: "13.234.154.152",
-        port: "9000",
-        pathname: "/cish-bucket/**",
-      },
+      }
     ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          { type: 'host', value: 'cish-web-steel.vercel.app' },
+        ],
+        destination: 'https://cish.org.in/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
